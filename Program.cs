15ec1d158251acdc0127c8dcb9fa5a523f256105
CVieiraSantos <cadu.vieira2276@gmail.com>
2022-07-19 16:23:57 -0300

    Invertendo os caracteres de uma string

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+using System;
+
+namespace Exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+                        
+            string frase = Console.ReadLine();
+
+            string resultado = InverterCaractere(frase);
+
+            Console.WriteLine(resultado.ToUpper());  
+
+        }
+
+       
+
+        public static string InverterCaractere(string inverterCaractere)
+        {
+            var inverter = new char[inverterCaractere.Length];
+            for (int i = inverterCaractere.Length - 1, destIndex = 0; i >= 0; i--, destIndex++)
+            {
+                inverter[destIndex] = inverterCaractere[i];
+            }
+            return new string(inverter);
+       }   
+    }
+}
